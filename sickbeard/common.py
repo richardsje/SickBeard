d52dcf127ff89d2dc31830e41763fc8ad0f78f20
richardsje <jamsandjax@gmail.com>
2012-10-08 20:28:25 +0200

    Update sickbeard/common.py
    Added 1080p WEB-DL quality option.

--- sickbeard/common.py
+++ sickbeard/common.py
@@ -61,7 +61,8 @@ class Quality:
     HDTV = 1<<2 # 4
     HDWEBDL = 1<<3 # 8
     HDBLURAY = 1<<4 # 16
-    FULLHDBLURAY = 1<<5 # 32
+    FULLHDWEBDL = 1<<5 # 32
+    FULLHDBLURAY = 1<<6 # 64
 
     # put these bits at the other end of the spectrum, far enough out that they shouldn't interfere
     UNKNOWN = 1<<15
@@ -73,6 +74,7 @@ class Quality:
                       HDTV: "HD TV",
                       HDWEBDL: "720p WEB-DL",
                       HDBLURAY: "720p BluRay",
+                      FULLHDWEBDL: "1080p WEB-DL",
                       FULLHDBLURAY: "1080p BluRay"}
 
     statusPrefixes = {DOWNLOADED: "Downloaded",
@@ -134,6 +136,8 @@ class Quality:
             return Quality.HDWEBDL
         elif checkName(["720p", "bluray", "x264"], all) or checkName(["720p", "hddvd", "x264"], all):
             return Quality.HDBLURAY
+        elif checkName(["1080p", "web.dl"], all) or checkName(["1080p", "itunes", "h.?264"], all):
+            return Quality.FULLHDWEBDL
         elif checkName(["1080p", "bluray", "x264"], all) or checkName(["1080p", "hddvd", "x264"], all):
             return Quality.FULLHDBLURAY
         else:
